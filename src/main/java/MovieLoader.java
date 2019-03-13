573e4822c27c12e0bd8f194c0010f0b3fc54f941
Mike <maf152@case.edu>
2019-03-12 21:46:55 -0400

    Skeleton of MovieLoader class

--- src/main/java/MovieLoader.java
+++ src/main/java/MovieLoader.java
@@ -1,6 +1,27 @@
+import java.util.List;
+
+
 public class MovieLoader {
 
 
+    public List<Movie> loadMovies(UISearch uiSearch){
+        //TODO
+        return null;
+    }
+
+    public Movie loadMovie(UISearch uiSearch ){
+        //TODO
+        return null;
+    }
+
+    public boolean open(){
+        //TODO
+        return false;
+    }
+
+    public boolean close(){
+        //TODO
+        return false;
+    }
 
 }
-
