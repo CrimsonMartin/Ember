3e394b8ac739cc92e6e8c3a40841e7417dc31777
ZacharyKadish <zachk@mindspring.com>
2019-03-24 17:17:08 -0400

    SearchOptionsActivity almost fully complete!
    - Fixed all the layout bugs lol (there were a lot)
    - Added RadioGroup that functions both as a filter display and a delete selection
    - Added delete button, while I was at it
    - Looks great, runs great (on Nexus One)
    - Reset button works better now, plus resets the second RadioGroup too
    
    Things still to do with this page:
    
    - Search feature, obviously
    - Restrict user to five filters at a time
    - Testing and restructuring for other emulators besides Nexus One
    - Figure out what's going on with all those warnings

--- app/src/main/java/com/group395/ember/SearchOptionsActivity.java
+++ app/src/main/java/com/group395/ember/SearchOptionsActivity.java
@@ -1,25 +1,31 @@
 package com.group395.ember;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import static com.group395.ember.FilterType.ACTOR;
+import static com.group395.ember.FilterType.DIRECTOR;
+import static com.group395.ember.FilterType.GENRE;
 
 
 public class SearchOptionsActivity extends AppCompatActivity {
 
-    //allowed number of filters before extra filters will not be applied
-    private final int allowableFilters = 5;
-    private Filter[] myFilters = new Filter[allowableFilters];
+
+    private int currentNumFilters = 0;
+    private Filter[] myFilters = new Filter[3];
     private FilterType selected = null;
-    //contains IDs of extra textviews generated to display additional filters
-    Integer[] genreIds = new Integer[allowableFilters];
-    Integer[] actorIds = new Integer[allowableFilters];
-    Integer[] directorIds = new Integer[allowableFilters];
+    private enum RadioFive{
+        RADIO_A, RADIO_B, RADIO_C, RADIO_D, RADIO_E;
+    }
+    private RadioFive selectedRadio = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,136 +36,176 @@ public class SearchOptionsActivity extends AppCompatActivity {
     protected void genreOnClick(View v){
         EditText editText = findViewById(R.id.editText);
         editText.setText("Enter genre.");
-        selected = FilterType.GENRE;
+        selected = GENRE;
     }
 
     protected void actorOnClick(View v){
         EditText editText = findViewById(R.id.editText);
         editText.setText("Enter actor.");
-        selected = FilterType.ACTOR;
+        selected = ACTOR;
     }
 
     protected void directorOnClick(View v){
         EditText editText = findViewById(R.id.editText);
         editText.setText("Enter director.");
-        selected = FilterType.DIRECTOR;
+        selected = DIRECTOR;
+    }
+
+    protected void radioAOnClick(View v){
+        selectedRadio = RadioFive.RADIO_A;
+    }
+    protected void radioBOnClick(View v){
+        selectedRadio = RadioFive.RADIO_B;
+    }
+    protected void radioCOnClick(View v){
+        selectedRadio = RadioFive.RADIO_C;
+    }
+    protected void radioDOnClick(View v){
+        selectedRadio = RadioFive.RADIO_D;
+    }
+    protected void radioEOnClick(View v){
+        selectedRadio = RadioFive.RADIO_E;
     }
 
     protected void submitOnClick(View v){
         //If no FilterType has been selected, do nothing.
         if(selected == null){
             return;
-
         }
         checkNumFilters();
         EditText editText = findViewById(R.id.editText);
+        boolean inserted = false;
         //If the FilterType is already in myFilters, add the filter to that Filter.
-        for(int i = 0; i < myFilters.length; i++){
+        for(int i = 0; i < 3; i++){
             if(myFilters[i] != null){
-                if(myFilters[i].getFilterType().equals(selected)){
+                if((inserted == false) && (myFilters[i].getFilterType().equals(selected))){
                     myFilters[i].add(editText.getText().toString());
+                    inserted = true;
                 }
             }
         }
-        //find first empty slot in arrays, if any. otherwise firstEmpty = -1
-        int firstEmpty = findFirstEmpty(myFilters);
-        myFilters[firstEmpty] = new Filter(selected);
-        String[] inputString = new String[1];
-        inputString[0] = editText.getText().toString();
+        //If there is no Filter of the proper FilterType, make a new one.
+        if(inserted == false) {
+            //find first empty slot in arrays, if any. otherwise firstEmpty = -1
+            int firstEmpty = findFirstEmpty(myFilters);
+            myFilters[firstEmpty] = new Filter(selected);
+            String[] inputString = new String[1];
+            inputString[0] = editText.getText().toString();
 
-        switch(selected){
-            case GENRE: myFilters[firstEmpty].setGenres(inputString);
-            case ACTOR: myFilters[firstEmpty].setActors(inputString);
-            case DIRECTOR: myFilters[firstEmpty].setDirectors(inputString);
+            switch (selected) {
+                case GENRE:
+                    myFilters[firstEmpty].setGenres(inputString);
+                    break;
+                case ACTOR:
+                    myFilters[firstEmpty].setActors(inputString);
+                    break;
+                case DIRECTOR:
+                    myFilters[firstEmpty].setDirectors(inputString);
+                    break;
+            }
         }
-
 
         //add filter to display of filters
-        TextView genreView = findViewById(R.id.genreFilters);
-        genreIds[0] = R.id.genreFilters;
-        TextView actorView = findViewById(R.id.actorFilters);
-        actorIds[0] = R.id.genreFilters;
-        TextView directorView = findViewById(R.id.directorFilters);
-        directorIds[0] = R.id.directorFilters;
-        switch(selected){
-            case GENRE:
-                if(genreView.getText().toString().equals(null)){
-                    genreView.setText(editText.getText().toString());
-                }else{
-                    TextView genreViewAdditional = new TextView(this);
-                    int idLocation = findFirstEmpty(genreIds);
-                    genreIds[idLocation] = View.generateViewId();
-                    genreViewAdditional.setId(genreIds[idLocation]);
-                    genreViewAdditional.setText(editText.getText().toString());
-                    ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
-                    constraintLayout.addView(genreViewAdditional);
-                    ConstraintSet constraintSet = new ConstraintSet();
-                    constraintSet.clone(constraintLayout);
-                    constraintSet.connect(genreIds[idLocation].intValue(), ConstraintSet.TOP, genreIds[idLocation - 1].intValue(), ConstraintSet.BOTTOM, 16);
-                    constraintSet.connect(genreIds[idLocation].intValue(), ConstraintSet.LEFT, R.id.radioGroupGAD, ConstraintSet.RIGHT, 16);
-                    constraintSet.connect(genreIds[idLocation].intValue(), ConstraintSet.RIGHT, R.id.constraintLayout, ConstraintSet.RIGHT, 16);
-                    constraintSet.connect(genreIds[idLocation].intValue(), ConstraintSet.BOTTOM, R.id.actorFilters, ConstraintSet.TOP, 16);
-                    constraintSet.connect(actorView.getId(), ConstraintSet.TOP, genreIds[idLocation].intValue(), ConstraintSet.BOTTOM, 16);
-                    constraintSet.applyTo(constraintLayout);
-                }
-            case ACTOR:
-                if(actorView.getText().toString().equals(null)){
-                    actorView.setText(editText.getText().toString());
-                }else{
-                    TextView actorViewAdditional = new TextView(this);
-                    int idLocation = findFirstEmpty(actorIds);
-                    actorIds[idLocation] = View.generateViewId();
-                    actorViewAdditional.setId(View.generateViewId());
-                    actorViewAdditional.setText(editText.getText().toString());
-                    ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
-                    constraintLayout.addView(actorViewAdditional);
-                    ConstraintSet constraintSet = new ConstraintSet();
-                    constraintSet.clone(constraintLayout);
-                    constraintSet.connect(actorIds[idLocation].intValue(), ConstraintSet.TOP, actorIds[idLocation - 1].intValue(), ConstraintSet.BOTTOM, 16);
-                    constraintSet.connect(actorIds[idLocation].intValue(), ConstraintSet.LEFT, R.id.radioGroupGAD, ConstraintSet.RIGHT, 16);
-                    constraintSet.connect(actorIds[idLocation].intValue(), ConstraintSet.RIGHT, R.id.constraintLayout, ConstraintSet.RIGHT, 16);
-                    constraintSet.connect(actorIds[idLocation].intValue(), ConstraintSet.BOTTOM, R.id.directorFilters, ConstraintSet.TOP, 16);
-                    constraintSet.connect(directorView.getId(), ConstraintSet.TOP, actorIds[idLocation].intValue(), ConstraintSet.BOTTOM, 16);
-                    constraintSet.applyTo(constraintLayout);
-                }
-            case DIRECTOR:
-                if(directorView.getText().toString().equals(null)){
-                    directorView.setText(editText.getText().toString());
-                }else{
-                    TextView directorViewAdditional = new TextView(this);
-                    int idLocation = findFirstEmpty(directorIds);
-                    directorIds[idLocation] = View.generateViewId();
-                    directorViewAdditional.setId(View.generateViewId());
-                    directorViewAdditional.setText(editText.getText().toString());
-                    ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
-                    constraintLayout.addView(directorViewAdditional);
-                    ConstraintSet constraintSet = new ConstraintSet();
-                    constraintSet.clone(constraintLayout);
-                    constraintSet.connect(directorIds[idLocation].intValue(), ConstraintSet.TOP, directorIds[idLocation - 1].intValue(), ConstraintSet.BOTTOM, 16);
-                    constraintSet.connect(directorIds[idLocation].intValue(), ConstraintSet.LEFT, R.id.radioGroupGAD, ConstraintSet.RIGHT, 16);
-                    constraintSet.connect(directorIds[idLocation].intValue(), ConstraintSet.RIGHT, R.id.constraintLayout, ConstraintSet.RIGHT, 16);
-                    constraintSet.connect(directorIds[idLocation].intValue(), ConstraintSet.BOTTOM, R.id.sendButton, ConstraintSet.TOP, 16);
-                    constraintSet.connect(R.id.sendButton, ConstraintSet.TOP, directorIds[idLocation].intValue(), ConstraintSet.BOTTOM, 16);
-                    constraintSet.applyTo(constraintLayout);
-                }
+        RadioButton displayRadio = findViewById(R.id.filterDisplayA);
+        switch(currentNumFilters){
+            case 0: displayRadio = findViewById(R.id.filterDisplayA);
+                    break;
+            case 1: displayRadio = findViewById(R.id.filterDisplayB);
+                    break;
+            case 2: displayRadio = findViewById(R.id.filterDisplayC);
+                    break;
+            case 3: displayRadio = findViewById(R.id.filterDisplayD);
+                    break;
+            case 4: displayRadio = findViewById(R.id.filterDisplayE);
+                    break;
         }
+        displayRadio.setText(editText.getText().toString());
+        currentNumFilters++;
 
 
     }
 
     protected void resetOnClick(View v){
-        myFilters = new Filter[2];
+        myFilters = new Filter[3];
         selected = null;
         EditText editText = findViewById(R.id.editText);
         editText.setText("Choose a filter.");
         RadioGroup radioGroupGRD = findViewById(R.id.radioGroupGAD);
         radioGroupGRD.clearCheck();
+        RadioButton displayRadio = findViewById(R.id.filterDisplayA);
+        displayRadio.setText(null);
+        displayRadio = findViewById(R.id.filterDisplayB);
+        displayRadio.setText(null);
+        displayRadio = findViewById(R.id.filterDisplayC);
+        displayRadio.setText(null);
+        displayRadio = findViewById(R.id.filterDisplayD);
+        displayRadio.setText(null);
+        displayRadio = findViewById(R.id.filterDisplayE);
+        displayRadio.setText(null);
+        currentNumFilters = 0;
     }
 
     protected void searchOnClick(View v){
         UISearch mySearch = new UISearch();
 
     }
+
+    protected void deleteOnClick(View v){
+        RadioButton displayRadio;
+        RadioButton displayRadioTemp;
+        switch(selectedRadio){
+            case RADIO_A:
+                displayRadio = findViewById(R.id.filterDisplayA);
+                displayRadioTemp = findViewById(R.id.filterDisplayB);
+                displayRadio.setText(displayRadioTemp.getText().toString());
+                displayRadio = findViewById(R.id.filterDisplayB);
+                displayRadioTemp = findViewById(R.id.filterDisplayC);
+                displayRadio.setText(displayRadioTemp.getText().toString());
+                displayRadio = findViewById(R.id.filterDisplayC);
+                displayRadioTemp = findViewById(R.id.filterDisplayD);
+                displayRadio.setText(displayRadioTemp.getText().toString());
+                displayRadio = findViewById(R.id.filterDisplayD);
+                displayRadioTemp = findViewById(R.id.filterDisplayE);
+                displayRadio.setText(displayRadioTemp.getText().toString());
+                displayRadioTemp.setText(null);
+                break;
+            case RADIO_B:
+                displayRadio = findViewById(R.id.filterDisplayB);
+                displayRadioTemp = findViewById(R.id.filterDisplayC);
+                displayRadio.setText(displayRadioTemp.getText().toString());
+                displayRadio = findViewById(R.id.filterDisplayC);
+                displayRadioTemp = findViewById(R.id.filterDisplayD);
+                displayRadio.setText(displayRadioTemp.getText().toString());
+                displayRadio = findViewById(R.id.filterDisplayD);
+                displayRadioTemp = findViewById(R.id.filterDisplayE);
+                displayRadio.setText(displayRadioTemp.getText().toString());
+                displayRadioTemp.setText(null);
+                break;
+            case RADIO_C:
+                displayRadio = findViewById(R.id.filterDisplayC);
+                displayRadioTemp = findViewById(R.id.filterDisplayD);
+                displayRadio.setText(displayRadioTemp.getText().toString());
+                displayRadio = findViewById(R.id.filterDisplayD);
+                displayRadioTemp = findViewById(R.id.filterDisplayE);
+                displayRadio.setText(displayRadioTemp.getText().toString());
+                displayRadioTemp.setText(null);
+                break;
+            case RADIO_D:
+                displayRadio = findViewById(R.id.filterDisplayD);
+                displayRadioTemp = findViewById(R.id.filterDisplayE);
+                displayRadio.setText(displayRadioTemp.getText().toString());
+                displayRadioTemp.setText(null);
+                break;
+            case RADIO_E:
+                displayRadio = findViewById(R.id.filterDisplayE);
+                displayRadio.setText(null);
+                break;
+        }
+        if(currentNumFilters > 0){
+            currentNumFilters--;
+        }
+    }
+
 
     private void checkNumFilters(){
 
@@ -172,7 +218,6 @@ public class SearchOptionsActivity extends AppCompatActivity {
             }
         }
         return -1;
-
     }
 
 }
