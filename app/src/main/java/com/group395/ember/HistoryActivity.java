64f10b44f31bc00db997ba946bbc1924e2428952
ZacharyKadish <zachk@mindspring.com>
2019-03-28 15:00:45 -0400

    HistoryActivity Version 0.2
    - Converted recentClicks from Movie[] to Movie[][]
    - Reworked addClick so that Movies can be accessed FILO

--- app/src/main/java/com/group395/ember/HistoryActivity.java
+++ app/src/main/java/com/group395/ember/HistoryActivity.java
@@ -1,17 +1,24 @@
 package com.group395.ember;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.Stack;
+
 public class HistoryActivity extends AppCompatActivity {
 
-    //TODO: Replace Movie[] with Stack for a FILO display (currently FIFO)
-    private static Movie[] recentClicks = new Movie[8];
-    //This field specifies how many Movies have been moved past by the "next" button.
-    private int displacement = 0;
+
+    private static Movie[][] recentClicks = new Movie[5][8];
+    //This field specifies how many sets of 8 Movies have been moved past by the "next" button.
+    private int pagesSkipped = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,34 +39,65 @@ public class HistoryActivity extends AppCompatActivity {
     public void tileDROnClick(View v) { tileOnClick(7); }
 
     public void previousOnClick(View v){
-        if(displacement >= 8) {
-            displacement -= 8;
+        if(pagesSkipped > 0) {
+            pagesSkipped --;
             displayAll();
         }
     }
     public void nextOnClick(View v){
-        if(recentClicks.length <= displacement + 8){
+        if(pagesSkipped + 1 >= recentClicks.length){
             return;
         }
-        displacement += 8;
+        pagesSkipped ++;
         displayAll();
     }
 
     private void tileOnClick(int whichButton){
-        MoviePageActivity.setCurrentMovie(recentClicks[whichButton]);
+        MoviePageActivity.setCurrentMovie(recentClicks[pagesSkipped][whichButton]);
         startActivity(new Intent(HistoryActivity.this, MoviePageActivity.class));
     }
 
-    protected static void addClick(Movie clickedMovie){
-        int firstEmpty = SearchOptionsActivity.findFirstEmpty(recentClicks);
-        if(firstEmpty == -1){
-            Movie[] tempClicks = new Movie[recentClicks.length + 8];
-            for(int i = 0; i < recentClicks.length; i++){
-                tempClicks[i] = recentClicks[i];
+    protected static void addClick(Movie clickedMovie) {
+        int firstEmptyArray = -1;
+        int firstEmptyIndex = -1;
+        //Find first empty spot in recentClicks, designated [firstEmptyArray][firstEmptyIndex].
+        for (int i = 0; i < recentClicks.length; i++) {
+            if (firstEmptyArray == -1) {
+                firstEmptyIndex = SearchOptionsActivity.findFirstEmpty(recentClicks[i]);
+                if (firstEmptyIndex != -1) {
+                    firstEmptyArray = i;
+                }
             }
-            recentClicks = tempClicks;
         }
-        recentClicks[firstEmpty] = clickedMovie;
+        //If the entire array is full, expand it by one row.
+        if (firstEmptyArray == -1) {
+            Movie[][] tempRecentClicks = new Movie[recentClicks.length + 1][8];
+            for(int i = 0; i < recentClicks.length; i++) {
+                for(int j = 0; j < 8; j++) {
+                    tempRecentClicks[i][j] = recentClicks[i][j];
+                }
+            }
+            recentClicks = tempRecentClicks;
+        }
+        //If the entire array is empty, insert clickedMovie into the first slot.
+        if ((firstEmptyArray == 0) && (firstEmptyIndex == 0)) {
+            recentClicks[0][0] = clickedMovie;
+            return;
+        }
+        for (int i = firstEmptyArray; i >= 0; i--) {
+            //If all subarrays are either full or empty, copy the last entry in the last row to a new row before proceeding.
+            if (firstEmptyIndex == 0) {
+                recentClicks[firstEmptyArray][firstEmptyIndex] = recentClicks[firstEmptyArray - 1][7];
+            }
+            for (int j = 7; j > 0; j--) {
+                recentClicks[i][j] = recentClicks[i][j - 1];
+            }
+            if (i > 0) {
+                recentClicks[i][0] = recentClicks[i - 1][7];
+            } else {
+                recentClicks[0][0] = clickedMovie;
+            }
+        }
     }
 
     private void display(Button button, Movie movie){
@@ -67,14 +105,14 @@ public class HistoryActivity extends AppCompatActivity {
         button.setText(movie.getTitle());
     }
     private void displayAll(){
-        display((Button) findViewById(R.id.tileAL), recentClicks[displacement]);
-        display((Button) findViewById(R.id.tileAR), recentClicks[1 + displacement]);
-        display((Button) findViewById(R.id.tileBL), recentClicks[2 + displacement]);
-        display((Button) findViewById(R.id.tileBR), recentClicks[3 + displacement]);
-        display((Button) findViewById(R.id.tileCL), recentClicks[4 + displacement]);
-        display((Button) findViewById(R.id.tileCR), recentClicks[5 + displacement]);
-        display((Button) findViewById(R.id.tileDL), recentClicks[6 + displacement]);
-        display((Button) findViewById(R.id.tileDR), recentClicks[7 + displacement]);
+        display((Button) findViewById(R.id.tileAL), recentClicks[pagesSkipped][0]);
+        display((Button) findViewById(R.id.tileAR), recentClicks[pagesSkipped][1]);
+        display((Button) findViewById(R.id.tileBL), recentClicks[pagesSkipped][2]);
+        display((Button) findViewById(R.id.tileBR), recentClicks[pagesSkipped][3]);
+        display((Button) findViewById(R.id.tileCL), recentClicks[pagesSkipped][4]);
+        display((Button) findViewById(R.id.tileCR), recentClicks[pagesSkipped][5]);
+        display((Button) findViewById(R.id.tileDL), recentClicks[pagesSkipped][6]);
+        display((Button) findViewById(R.id.tileDR), recentClicks[pagesSkipped][7]);
     }
 
 }
