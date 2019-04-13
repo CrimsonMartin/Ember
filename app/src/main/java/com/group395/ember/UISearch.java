baac07bff9a41a68aaead30aef94fd47c852e95e
ZacharyKadish <zachk@mindspring.com>
2019-04-13 19:21:18 -0400

    Bugfixes - Removed "-1" years - Fixed null results crash

--- app/src/main/java/com/group395/ember/UISearch.java
+++ app/src/main/java/com/group395/ember/UISearch.java
@@ -213,7 +213,7 @@ public class UISearch {
             output[1] = results.get(pagesSkipped * 2 + 1);
         }
         catch (IndexOutOfBoundsException | InterruptedException e){
-            output = null;
+            output = new Movie[]{null, null};
         }
 
         return output;
